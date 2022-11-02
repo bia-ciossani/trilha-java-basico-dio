dc58350c5848c58a9dc9af9483bfbad454e1f77a
perkles <b.ciossani@outlook.com>
2022-11-02 14:12:33 -0300

    Atualização fator ternário

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,19 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        String resultado = a==b ?"verdadeiro" : "falso";
+
+        System.out.println(resultado);
+
+// Usando expressão booleana e operador ternário.       
+// Assim que detecta uma string, não faz as operações matemáticas, exceto por comanto em parenteses. 
+// Para tornar um número negativo em positivo, usar *-1.
+
+        
+    }
+    
+}
